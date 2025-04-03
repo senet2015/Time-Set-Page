96c719b977e29e1b62bb4e0c21ccc2ad5d96735d
senet2015 <senet@naver.com>
2025-04-04 03:20:20 +0900

    second commit

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-date-time-ntp-client-server-arduino/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+#include <Arduino.h>
+#include <WiFi.h>
+#include "time.h"
+
+//const char* ssid     = "hotspot7";
+const char* ssid     = "iptime_hunslab";
+const char* password = "87654321";
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600*9; // GMT+9 for Korea
+const int   daylightOffset_sec = 0;
+
+void printLocalTime();
+
+void setup(){
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  delay(1000);
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Connecting to NTP server...");
+  Serial.println(ntpServer);
+  Serial.println("Time zone: GMT+9");
+  Serial.println("Daylight offset: 0");
+  Serial.println("NTP server: ");
+
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
+
+void loop(){
+  delay(1000);
+  printLocalTime();
+}
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+}
